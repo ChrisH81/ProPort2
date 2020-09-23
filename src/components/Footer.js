e5e65bca6299bedf3015263babb6fd5d8087e0f5
Chris H <kekaneone@gmail.com>
2020-09-23 15:32:35 -0600

    add in comp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Footer = () => (
-    <div className="text-center center-block bg-light">
+    <footer className="text-center center-block bg-light">
         <a href="https://www.github.com/"><i className="fa fa-github fa-2x social"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="https://stackoverflow.com/"><i className="fa fa-stack-overflow fa-2x social"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="https://linkedin.com/"><i className="fa fa-linkedin-square fa-2x social"></i></a>
-    </div> 
+    </footer> 
 );
 
 export default Footer; 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,96 @@
  
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Pic from './300.png'; 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const Portfolio = () => (
     <div>
-        Portfolio
+        <h3>Portfolio</h3>
+        
+        <Card style={{ width: '25rem', float: 'left' }}>
+            <Card.Img variant="top" src={Pic} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Link variant="primary">Go to website</Link>
+                <br/>
+                <Link variant="primary">Go to github</Link>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem', float: 'left' }}>
+            <Card.Img variant="top" src={Pic} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Link variant="primary">Go to website</Link>
+                <br/>
+                <Link variant="primary">Go to github</Link>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem', float: 'left' }}>
+            <Card.Img variant="top" src={Pic} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Link variant="primary">Go to website</Link>
+                <br/>
+                <Link variant="primary">Go to github</Link>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem', float: 'left' }}>
+            <Card.Img variant="top" src={Pic} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Link variant="primary">Go to website</Link>
+                <br/>
+                <Link variant="primary">Go to github</Link>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem', float: 'left' }}>
+            <Card.Img variant="top" src={Pic} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Link variant="primary">Go to website</Link>
+                <br/>
+                <Link variant="primary">Go to github</Link>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem', float: 'left' }}>
+            <Card.Img variant="top" src={Pic} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Link variant="primary">Go to website</Link>
+                <br/>
+                <Link variant="primary">Go to github</Link>
+            </Card.Body>
+        </Card>
     </div> 
 );
 
