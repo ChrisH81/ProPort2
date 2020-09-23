af7a1b175f4feb04ed1f5162a32dca01d13ab204
Chris H <kekaneone@gmail.com>
2020-09-23 17:30:58 -0600

    update

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => (
     <div>
         <h3>About Me</h3>
         <img src={Pic} />
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <p>Hello I am just an average guy that does average computer stuff. I enjoy music and friends. We live in strange times.</p>
     </div>  
 );
 
